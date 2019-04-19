d2cf57d8ca5be3f2fe542c93542aa1ce5e713a3d
Shvili <sergo@shvili.me>
2019-04-19 12:31:34 +0300

    Append settings

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""
+Settings for Feedback project.
+"""
+
+import os
+
+
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8080
+ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'feedback')
